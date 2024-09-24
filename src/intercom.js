c95c0bbd88a3688d0444335481ab195c9a1cf4dd
Bowen Cheng <bowencheng2005@gmail.com>
2024-09-23 23:39:09 -0500

    change intercom account

--- src/intercom.js
+++ src/intercom.js
@@ -3,7 +3,8 @@ import Intercom from '@intercom/messenger-js-sdk';
 
 export default function Component() {
   Intercom({
-    app_id: 'zeoovopy',
+   // app_id: 'zeoovopy',
+   app_id: 'osdluh2g',
   });
 
   return <div>Ignore this. Click on the app on the bottom right.</div>;
